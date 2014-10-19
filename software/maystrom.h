b139b059f6d9efe952f2fe45f773aa7f8c85c7c1
David May <david.may.muc@googlemail.com>
2014-10-19 19:46:39 +0200

    minor

--- software/maystrom.h
+++ software/maystrom.h
@@ -9,13 +9,30 @@
 #include "fonts/Arial28x28.h"
 #include "fonts/Arial12x12.h"
 
+struct config_handle {
+    float aquisitionInterval;
+    float promotionInterval;
+
+};
+
+struct data_handle {
+    float temp;
+};
+
+
 struct board_handle {
+    config_handle *config;
+    data_handle *data;
     DigitalOut *notification_led;
-    DigitalOut *error_led; 
+    DigitalOut *error_led;
     SLCD *slcd;
     Serial *ble;
     Serial *pc;
     PARALLEL_TFT_ILI9341 *tft;
-    };
+    Ticker * aquisitionTicker;
+    Ticker * promotionTicker;
+};
+
+
 
 
