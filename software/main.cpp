828dccaf19ede7a7db57d4183a85e2d11f227b17
David May <david.may.muc@googlemail.com>
2014-10-19 19:44:26 +0200

    added structure of main programm flow

--- software/main.cpp
+++ software/main.cpp
@@ -1,47 +1,78 @@
-/** 
-maystrom 2014 
-david.may.muc@gmail.com 
+/**
+maystrom 2014
+david.may.muc@gmail.com
 */
 #include "maystrom.h"
 
 #include "hal/startup_hal.h"
 #include "hal/notification_hal.h"
-#include "hal/lcdmenu_hal.h"
+#include "hal/aquisition_hal.h"
+#include "hal/promotion_hal.h"
+#include "hal/communication_hal.h"
+#include "hal/configuration_hal.h"
 
 
-    
 
 struct board_handle * bh;
 
-void sendStatus() {
-        uint8_t type=0;
-        bh->ble->putc(type);
-        bh->ble->putc(rand());
-    }
+volatile uint8_t do_aquisition;
+volatile uint8_t do_promotion;
 
 int main(void)
 {
     int r;
-    Ticker statusTicker;
-    
-    /* startup */ 
+
+    /* startup */
     r = startup_board(&bh);
     if (r>0) error(bh,"%s ERRNO: %i\n",__FUNCTION__,r);
-    
-    
-    
 
-    statusTicker.attach(&sendStatus,1.0);
-    
-    while (1) {}
-/*
-    while (1) {
-        if(ble->readable()) {
-            pc.putc(ble->getc());
+    /* load configuration
+    // SD-Card
+    */
+    initConfiguration(bh);
+    debug(bh,"configuration init done\n");
+
+    /* init local lcd menu
+    // push button and timer interrupt
+    */
+
+    /* ctrl communication
+    // interrupt based
+    */
+    initCommunication(bh);
+    debug(bh,"communication init done\n");
+
+
+    /* aquire data
+    // timer based interupt
+    */
+    initAquisition(bh);
+    debug(bh,"aquisition init done\n");
+
+
+    /* promote data
+    // timer based interrupt
+    */
+    initPromotion(bh);
+    debug(bh,"promotion init done\n");
+
+
+    while(1) {
+
+        if (do_aquisition==1) {
+            do_aquisition = 0;
+            aquireData(bh);
         }
+
+        if (do_promotion==1) {
+            do_promotion=0;
+            promoteData(bh);
+        }
+
     }
-    
-*/
+
+
+
 
 
 }
